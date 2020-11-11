41869a2fbf4a67cd68cbadff90dacff69910eee9
M-Hachi <emoriuchi08@gmail.com>
2020-11-11 20:47:27 +0900

    Initial Commit

---
+++ VerticalSlider/VerticalSlider.h
@@ -0,0 +1,14 @@
+// Created 2020/11/11
+// Using Swift 5.0
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for VerticalSlider.
+FOUNDATION_EXPORT double VerticalSliderVersionNumber;
+
+//! Project version string for VerticalSlider.
+FOUNDATION_EXPORT const unsigned char VerticalSliderVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <VerticalSlider/PublicHeader.h>
+
+
